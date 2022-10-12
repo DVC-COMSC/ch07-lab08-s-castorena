edb6b529429e97728cbb045b2b819a8757cfa96c
sarah castorena <ceanz@yahoo.com>
2022-10-12 13:33:11 -0700

    New commit

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 # ******************************
 # Make your Code
 # ******************************
+# Sarah Castorena
+# SID
+numbers = []
+count = 0
+while count < 5:
+    num = int(input('Enter numbers in acending order'))
+    numbers.append(num) 
+    count = count + 1
+
+insertNum = int(input('Enter the insert number'))
+if insertNum > numbers[4]:
+   numbers.append(insertNum)
+elif insertNum < numbers[0]:
+    numbers.insert(0,insertNum)   
+for i in range(len(numbers)-1):
+    
+    if (insertNum > numbers[i]) and (insertNum < numbers[i+1]) :
+        x = numbers.index(numbers[i])
+        numbers.insert(x+1, insertNum)
+    
+     
+
+
+print(numbers)
